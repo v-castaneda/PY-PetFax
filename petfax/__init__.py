af3e8fe691544475295f58997208720b870297f2
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-12-10 00:02:12 -0500

    updating files

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,10 +1,20 @@
 # importing flask
 from flask import Flask
+from flask_migrate import Migrate
 
 # defining function for app factory
 def create_app():
     app = Flask(__name__)
 
+    # database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:hello123@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+    # index route
     @app.route('/')
     def hello():
         return 'Hello, PetFax!'
